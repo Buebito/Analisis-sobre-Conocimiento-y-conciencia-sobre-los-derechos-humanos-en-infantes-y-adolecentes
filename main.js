845cc19634b102f95ad7c53c1722499f0728db21
Rodrigo Uribe <rodrigo.uribe2001@gmail.com>
2024-04-21 10:58:26 -0600

    Las burbujas ahora contienen el texto de la respuesta. Se eliminó las respuestas nulas.

--- main.js
+++ main.js
@@ -6,17 +6,21 @@ async function cargarDatos() {
     const data = csvToArray(csvText, ',');
 
     // Procesa y agrega la edad convertida en número a cada objeto
-    datosProcesados = data.map(d => ({
-        respuesta: d["¿Qué son para ti los Derechos Humanos?"],
-        edad: parseFloat(d["¿Cuántos años cumplidos tienes? (anota el número)"]),
-        radius: Math.sqrt(d["¿Qué son para ti los Derechos Humanos?"].length) * 2,
-        color: 'gray' // Color por defecto, se asignará según el botón pulsado
-    }));
+    // Filtra los datos para descartar aquellos sin respuesta o edad
+    datosProcesados = data
+        .filter(d => d["¿Qué son para ti los Derechos Humanos?"] && d["¿Cuántos años cumplidos tienes? (anota el número)"])
+        .map(d => ({
+            respuesta: d["¿Qué son para ti los Derechos Humanos?"],
+            edad: parseFloat(d["¿Cuántos años cumplidos tienes? (anota el número)"]),
+            // Aquí se calcula el radio basado en la longitud de la respuesta
+            radius: Math.sqrt(d["¿Qué son para ti los Derechos Humanos?"].length) * 2,
+            color: 'gray' // Color por defecto, se asignará según el botón pulsado
+        }));
 
     console.log(datosProcesados);
 
     // Visualizar todos los datos al inicio
-    actualizarVisualizacion('menores10');
+    actualizarVisualizacion('todos');
 }
 
 // Función para convertir CSV a array de objetos
@@ -34,22 +38,29 @@ function csvToArray(str, delimiter = ",") {
 }
 
 function seleccionarAleatoriamente(arr, num = 15) {
-    let indices = arr.map((_, i) => i);
-    let seleccionados = [];
-
-    for (let i = 0; i < num; i++) {
-        if (indices.length === 0) {
-            throw new RangeError("seleccionarAleatoriamente: menos elementos disponibles que los solicitados");
-        }
-        let randomIndex = Math.floor(Math.random() * indices.length);
-        seleccionados.push(arr[indices[randomIndex]]);
-        indices.splice(randomIndex, 1); // Elimina el índice usado
+    const shuffledArray = [...arr];
+    let m = shuffledArray.length, t, i;
+  
+    // Mientras queden elementos a mezclar...
+    while (m) {
+      // Escoger un elemento restante...
+      i = Math.floor(Math.random() * m--);
+  
+      // Y cambiarlo por el elemento actual.
+      t = shuffledArray[m];
+      shuffledArray[m] = shuffledArray[i];
+      shuffledArray[i] = t;
     }
+  
+    return shuffledArray.slice(0, num);
+}
 
-    return seleccionados;
+function colorAleatorio() {
+    return `hsl(${Math.random() * 360}, 100%, 50%)`;
 }
 
 function actualizarVisualizacion(rangoEdad) {
+    
     // Filtra los datos según el rango de edad seleccionado
     let datosFiltrados;
     if (rangoEdad === 'menores10') {
@@ -68,8 +79,15 @@ function actualizarVisualizacion(rangoEdad) {
 
     let datosAleatorios = seleccionarAleatoriamente(datosFiltrados);
     console.log(datosAleatorios);
-    // Aumentar el radio mínimo si es necesario para acomodar el texto
-    datosAleatorios.forEach(d => d.radius = Math.max(40, d.radius));
+    
+
+    // Calcula el radio de las burbujas basado en el texto
+    const context = document.createElement('canvas').getContext('2d');
+    context.font = '16px sans-serif'; // Asegúrate de usar el mismo estilo de fuente que se usará en el SVG
+    datosAleatorios.forEach(d => {
+        const width = context.measureText(d.respuesta).width;
+        d.radius = Math.max(100, Math.sqrt(width * width) * 0.5); // Ajusta el factor para el tamaño de la burbuja
+    });
 
     let svg = d3.select('#viz').select('svg');
     if (svg.empty()) {
@@ -86,7 +104,7 @@ function actualizarVisualizacion(rangoEdad) {
 
     // Crear la simulación de fuerzas para las burbujas con datosAleatorios
     const simulation = d3.forceSimulation(datosAleatorios)
-        .force('charge', d3.forceManyBody().strength(5))
+        .force('charge', d3.forceManyBody().strength(10))
         .force('center', d3.forceCenter(width / 2, height / 2))
         .force('collision', d3.forceCollide().radius(d => d.radius));
 
@@ -97,6 +115,7 @@ function actualizarVisualizacion(rangoEdad) {
 
     // Eliminar las burbujas existentes antes de crear nuevas
     svg.selectAll('.bubble').remove();
+    svg.selectAll('.texto').remove();
     
 
     // Crear nuevas burbujas basadas en datosAleatorios
@@ -113,6 +132,16 @@ function actualizarVisualizacion(rangoEdad) {
 
     // Guardar la referencia a la simulación en el nodo SVG
     svg.node().__simulation = simulation;
+
+    const textos = svg.selectAll('.texto')
+        .data(datosAleatorios, d => d.respuesta)
+        .enter().append('text')
+        .attr('class', 'texto')
+        .text(d => d.respuesta)
+        .attr('fill', 'black')
+        .style('text-anchor', 'middle')
+        .style('alignment-baseline', 'middle')
+        .style('font-size', '16px');
 
     // Funciones para manejar los eventos de arrastre
     function dragstarted(event, d) {
@@ -138,6 +167,10 @@ function actualizarVisualizacion(rangoEdad) {
         svg.selectAll('.bubble')
             .attr('cx', d => d.x)
             .attr('cy', d => d.y);
+
+        svg.selectAll('.texto')
+            .attr('x', d => d.x)
+            .attr('y', d => d.y);
     }
 
     // Inicia la simulación con ticked
